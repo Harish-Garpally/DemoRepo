1150bfed89ab2567dfc8703f10eadc89ed423a74
Harish-Garpally <70568056+Harish-Garpally@users.noreply.github.com>
2020-09-16 17:49:01 +0530

    minor changes

--- src/main/java/com/qa/pages/Loginpage.java
+++ src/main/java/com/qa/pages/Loginpage.java
@@ -2,4 +2,15 @@ package com.qa.pages;
 
 public class Loginpage {
 
+	
+	public void loginpage() {
+		
+		System.out.println("loginpage");
+		
+		
+	}
+	
+public void login() {
+		
+		System.out.println("loginpage");
 }
---
+++ src/main/java/com/qa/pages/Searchpage.java
@@ -0,0 +1,10 @@
+package com.qa.pages;
+
+public class Searchpage {
+	
+	public void searchPage() {
+		
+		System.out.println("search page");
+	}
+
+}
